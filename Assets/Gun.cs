33950606d1b2dbd2b05d0ad679d26908dffec165
Zaida Kodzoeva <zkod777@gmail.com>
2022-12-23 17:26:19 +0400

    attempt at follow mouse movement

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//https://www.youtube.com/watch?v=LNLVOjbrQj4 6:57
 public class Gun : MonoBehaviour
 {
     public Transform BulletPlacement;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
+
+    public float moveSpeed = 5f;
+    public Rigidbody rb;
+    public Camera cam;
+
+    Vector3 movement;
+    Vector3 mousePos;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +24,21 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
         if (Input.GetKeyDown(KeyCode.Space)){
             var bullet = Instantiate(bulletPrefab, BulletPlacement.position, BulletPlacement.rotation);
             bullet.GetComponent<Rigidbody>().velocity = BulletPlacement.forward * bulletSpeed;
         }
         
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        Vector3 lookDir = mousePos - rb.position;
     }
 }
